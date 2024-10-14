4087b2553cf2d527230799a84ba78275b387b726
Dhung-2006 <dexterplayer2007@gmail.com>
2024-10-14 13:52:37 +0800

    123

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from excel_to_word import main
 # Create your views here.
 def index(request):
     return render(request, "index.html", locals())
+
+def click_button():
+    main.Cov()
