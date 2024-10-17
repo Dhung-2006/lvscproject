6dc859ac2b412ad71624942149437292e3092c97
allenhnn <allenhuang95710@gmail.com>
2024-10-17 17:13:42 +0800

    while迴圈跑不動 --Allen 1017

--- myapp/views.py
+++ myapp/views.py
@@ -3,14 +3,13 @@ from excel_to_word import main
 from django.http import HttpResponse
 def index(request):
     return render(request, "index.html", locals())
-
 def runConvert(request):
     if request.method == "POST":
-        main.Cov()
-<<<<<<< HEAD
-        return HttpResponse("123556")  # 简单返回
-    return HttpResponse("Invalid request", status=400)
-=======
-        return HttpResponse("123556")
-    return HttpResponse("Invalid request", status=400)
->>>>>>> f1da90f5d24d6b0f3b763fa9bf05e35fdc61e7ad
+        # main.Cov()
+        context = {
+        'State': "1",
+        }
+        render(request, 'index.html', context)
+        return HttpResponse("Done Process", status=200)
+    else:
+        return HttpResponse("Invalid request", status=400)
