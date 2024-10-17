29b3d7551708589148e955091c5e93cbbda101d8
Dhung-2006 <dexterplayer2007@gmail.com>
2024-10-18 01:28:44 +0800

    views.py update

--- myapp/views.py
+++ myapp/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 from excel_to_word import main
 from django.http import HttpResponse
+from django.http import JsonResponse 
 def index(request):
     return render(request, "index.html", locals())
 def runConvert(request):
     if request.method == "POST":
         # main.Cov()
         context = {
-        'State': "1",
+            'file':'result.pdf'
         }
-        render(request, 'index.html', context)
-        return HttpResponse("Done Process", status=200)
+        return JsonResponse(context)
     else:
         return HttpResponse("Invalid request", status=400)
+        
 
 # merge test
